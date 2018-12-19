0f6c3675c4cec2ed9e62cef14037d9b1983d5882
Arkadiusz Wierzbicki <sz144@wp.pl>
2018-12-19 20:34:15 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var btn = document.querySelector('#locked button');
 btn.addEventListener('click', passwordCheck);
 
 function passwordCheck(){
-    if(password.value=='zaczynam'){
+    if(password.value=='ZAQ!@WSXCDE#$RFVBGT%^YHNMJU&'){
         locked.style.display="none";
     }
 }
