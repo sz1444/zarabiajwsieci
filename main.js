bb16c30610e3be37ffcb5ce46ffedb18abc6b8e0
Arkadiusz Wierzbicki <sz144@wp.pl>
2018-12-19 19:11:32 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-var box = document.querySelector('.box');
-var login = document.querySelector('#login');
-var password= document.querySelector('#login input');
-var btn = document.querySelector('#login button');
+var locked = document.querySelector('.locked');
+var password= document.querySelector('#login');
+var btn = document.querySelector('#locked button');
+
 
 btn.addEventListener('click', passwordCheck);
 
 function passwordCheck(){
-    console.log(box);
     if(password.value=='zaczynam'){
-        login.style.display="none";
-        box.classList.remove('locked');
+        locked.style.display="none";
     }
 }
